80041c698a0fe7ee5b4b595616c599d9ee400153
Protap Ghosh <protapg518@gmail.com>
2023-04-30 23:51:35 +0600

    marquee via update

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -14,9 +14,12 @@ const Header = () => {
                 <p>{moment().format("dddd, MMMM Do YYYY, h:mm A")}</p>
             </div>
 
-            <div className='d-flex my-4 p-2 fs-5 rounded-1 bg-info bg-opacity-10'>
+            <div className='d-flex my-4 p-2 rounded-1 bg-info bg-opacity-10'>
                 <Button variant="danger">Latest</Button>
-                <Marquee className='text-danger' speed={70} pauseOnHover={true}>I can be a React component, multiple React components, or just some text...</Marquee>
+                <Marquee className='text-danger' speed={70} pauseOnHover={true}>
+                    <span className='me-5'>Tucker Carlson Swears Vladimir Putin Is Winning The War In Ukraine...<Link>Read More</Link></span>
+                    <span className='me-5'>US to name military mission in Ukraine and appoint commander - WSJ...<Link>Read More</Link></span>
+                </Marquee>
             </div>
 
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
