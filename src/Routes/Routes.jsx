f6f01bf9477c28c4a65a072a9a7c36b087761bc3
Protap Ghosh <protapg518@gmail.com>
2023-04-30 12:54:25 +0600

    add category component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
-import Discover from "../pages/Shared/Discover/Discover";
+import Category from "../pages/Home/Category/Category";
 
 const router = createBrowserRouter([
     {
@@ -11,12 +11,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>,
-                children: [
-                    {
-                        path: "/learn-more",
-                        element: <Discover></Discover>
-                    }
-                ]
+            },
+            {
+                path: '/categories/:id',
+                element: <Category></Category>,
             },
         ],
     },
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 import QZone from '../QZone/QZone';
-import Discover from '../Discover/Discover';
+import bg from '../../../assets/bg.png'
+import { Link } from 'react-router-dom';
 
 const RightNav = () => {
     return (
@@ -21,7 +22,16 @@ const RightNav = () => {
                     <ListGroup.Item action variant="danger" href="https://www.instagram.com"> <FaInstagram /> Instagram</ListGroup.Item>
                 </ListGroup>
                 <QZone></QZone>
-                <Discover></Discover>
+                <div className='position-relative text-white text-center'>
+                    <img src={bg} alt="" />
+                    <div className='position-absolute top-0 pt-4 mt-5 px-4'>
+                        <h3 className='lh-base fw-bold'>Create an Amazing Newspaper</h3>
+                        <p className='my-4'>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+                        <Link>
+                            <Button className='fs-5 py-3 px-4 rounded-0' variant="danger">Learn More</Button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
