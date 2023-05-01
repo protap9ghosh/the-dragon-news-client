c1fb28cc99be8d279e7e48619b3cccd71b172131
Protap Ghosh <protapg518@gmail.com>
2023-05-02 02:13:26 +0600

    setup private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
-import AuthProvider from '../providers/AuthProvider';
+import { AuthContext } from '../providers/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = () => {
-    const { user } = useContext(AuthProvider);
+const PrivateRoute = ({children}) => {
+    const { user } = useContext(AuthContext);
+    console.log(user);
+    const location = useLocation();
+    console.log(location);
 
-    return (
-        <div>
-            
-        </div>
-    );
+    if (user) {
+        return children;
+    }
+
+    return <Navigate state={{from: location}} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
@@ -19,5 +23,5 @@ export default PrivateRoute;
  * 1. Check user is logged in or not
  * 2. If user is logged in, then allow then to visit the route
  * 3. Else redirect the user to the login page
- * 
+ * 4. setup the private route
  * */
